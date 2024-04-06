25b94d3c8b1c694d2906b2731432dfe745f1e213
Ihimbru-k <kanihims25@gmail.com>
2024-04-06 15:54:06 +0100

    passing data around

--- lib/ui/screen5.dart
+++ lib/ui/screen5.dart
@@ -6,19 +6,65 @@ import 'package:intl/intl.dart';
 import 'package:form/ui/screen6.dart';
 
 class ParentDisplay extends StatelessWidget {
+
+  final String lessons;
+  final String courseLength;
+  final String lessonLength;
+  final String costPerLesson;
+  final String roadTestPkg;
+  final String totalAmount;
+  final String administrativeExpense;
+  final String courseRate;
+  final String payment11;
+  final String date11;
+  final String payment12;
+  final String date12;
+  final String payment21;
+  final String date21;
+  final String payment22;
+  final String date22;
+  final String payment31;
+  final String date31;
+  final String payment32;
+  final String date32;
+  final String payment41;
+  final String date41;
+  final String payment42;
+  final String date42;
+  final String lessons1;
+  final String courseLength1;
+  final String lessonLength1;
+  final String lessonCost1;
+  final String lessons2;
+  final String courseLength2;
+  final String lessonLength2;
+  final String lessonCost2;
+  final String courseRate1;
+  final String representativeName;
+  final Uint8List? repInitials;
+  final Uint8List? repSignature;
+ // final String selectedDate;
+  final String classroomInstructionStartTime;
+  final String classroomInstructionEndTime;
+  final String classroomInstructionStartDate;
+  final String makeupAssignment;
+
+
+
   final String parentName;
   final Uint8List? parentInitials;
   final Uint8List? parentSignature;
   final DateTime parentSigningDate;
-  final String representativeName;
+  //final String representativeName;
   final Uint8List? representativeInitials;
   final Uint8List? representativeSignature;
   final DateTime selectedDate;
-  final String classroomInstructionStartDate;
+  //final String classroomInstructionStartDate;
   final String classroomInstructionEndDate;
   final String makeupAssignmentDate;
   final Uint8List? initials;
   final Uint8List? signature;
+
 
   ParentDisplay({
     required this.parentName,
@@ -34,6 +80,44 @@ class ParentDisplay extends StatelessWidget {
     required this.makeupAssignmentDate,
     required this.initials,
     required this.signature,
+    required this.lessons,
+    required this.courseLength,
+    required this.lessonLength,
+    required this.costPerLesson,
+    required this.roadTestPkg,
+    required this.totalAmount,
+    required this.administrativeExpense,
+    required this.courseRate,
+    required this.payment11,
+    required this.date11,
+    required this.payment12,
+    required this.date12,
+    required this.payment21,
+    required this.date21,
+    required this.payment22,
+    required this.date22,
+    required this.payment31,
+    required this.date31,
+    required this.payment32,
+    required this.date32,
+    required this.payment41,
+    required this.date41,
+    required this.payment42,
+    required this.date42,
+    required this.lessons1,
+    required this.courseLength1,
+    required this.lessonLength1,
+    required this.lessonCost1,
+    required this.lessons2,
+    required this.courseLength2,
+    required this.lessonLength2,
+    required this.lessonCost2,
+    required this.courseRate1,
+    required this.repInitials,
+    required this.repSignature,
+    required this.classroomInstructionStartTime,
+    required this.classroomInstructionEndTime,
+    required this.makeupAssignment,
   });
 
   @override
@@ -70,6 +154,12 @@ class ParentDisplay extends StatelessWidget {
                         parentSigningDate: parentSigningDate,
                         initials: initials,
                         signature: signature,
+
+
+
+
+
+
                       ),
                     ),
                   );
